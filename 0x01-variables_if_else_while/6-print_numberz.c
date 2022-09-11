2c25044dda83b8c244c38a587c9832becdd2f555
Baruwa81 <baruwa_toheeb@yahoo.com>
2022-09-11 21:53:40 +0100

    print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int 0;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
+
+	putchar('\n');
+	return (0);
+}

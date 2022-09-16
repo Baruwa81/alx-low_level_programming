b0d067ada6ee50dd959dad15aabbedabcada6f19
Baruwa81 <baruwa_toheeb@yahoo.com>
2022-09-16 22:25:25 +0100

    largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,29 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
+ *  largest_number - Returns largest number
+ *  @a: first num
+ *  @b: second num
+ *  @c: third num
+ *
+ *  Return: largest number
  */
-
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a >= b && a >= c)
-{
-largest = a;
-}
-else if (b >= a && b >= c)
-{
-largest = b;
-}
-else if (c >= a && c >= b)
-{
-largest = c;
-}
-return (largest);
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,21 +1,18 @@
 #include "main.h"
-/**
- * _isupper - Evaluate if letter is uppercase.
- * @c: print int
- * Return: Always 0.
- */
 
+/**
+ * _isupper - define if a character is uppercase
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
+ */
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		else
+			return (0);
 }

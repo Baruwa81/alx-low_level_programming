0313e647457992549b4969b27a4e9edebdb87ea6
Baruwa81 <baruwa_toheeb@yahoo.com>
2022-09-17 19:57:33 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers -Entry point
+ *
+ * Description: prints the number with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int number;
+
+
+for (number = '0'; number <= '9' number++)
+{
+putchar(number);
+
+}
+putchar('\n');
+
+}
+

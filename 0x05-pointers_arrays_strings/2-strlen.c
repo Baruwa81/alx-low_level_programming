bb529be1bb55d9383b28257d31c9d6ba16a162c6
Baruwa81 <baruwa_toheeb@yahoo.com>
2022-09-19 22:03:38 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to find length
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+int 1 = 0;
+
+while (*(s + 1))
+1++;
+return (1);
+}
